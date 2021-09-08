612e17756d268ea02e1b196d448ad142cc6158b5
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-09-08 16:45:32 +0600

    db connected

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3vnz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express()
 const port = 5000;
 
-app.get('/',(req,res) => {
-    res.send('Hello Sudipta Acharjee!')
-})
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("educationSite").collection("subjects");
+  console.log("Database Connected");
+  
+});
+
 
 app.listen(port);
