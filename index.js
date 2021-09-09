037aeb5e9eb00fb5ee03003c8b26e6c07cb2eeca
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-09-09 10:29:41 +0600

    updated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3vnz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('service'));
 const port = 5000;
 
 
@@ -15,7 +18,11 @@ client.connect(err => {
   const services = client.db("educationSite").collection("subjects");
   app.post('/addService', (req, res => {
     const service = req.body;
-    services.insertMany()
+    console.log(service);
+    // services.insertOne(service)
+    // .then(result=>{
+    //   console.log(result)
+    // })
   }))
 });
 
